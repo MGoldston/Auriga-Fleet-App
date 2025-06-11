1e18e72f2bdffa042d8536bdb27516df265bc75b
MGoldston <mgoldston@outlook.com>
2025-06-11 15:17:32 +1000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,18 +1,2 @@
 // Web Version: Fleet Status App using React + Tailwind CSS
-import React, { useState, useEffect } from 'react';
-
-const helicopters = [
-  'VH-8Z3', 'VH-8Z4', 'VH-8Z9', 'VH-DBJ', 'VH-DKA',
-  'VH-XU8', 'VH-XU9', 'VH-XUV', 'VH-ZFD', 'VH-ZFM',
-  'VH-ZGH', 'VH-ZGP', 'VH-ZGZ'
-];
-
-const bases = [
-  'Karratha', 'Port Headland', 'Mackay', 'Hay Point', 'Gladstone', 'Newcastle', 'Other'
-];
-
-// ... [Full code truncated for space. Will be inserted fully during build]
-export default function App() {
-  // Content of App() from canvas
-  return <div className="p-4">[App content inserted here]</div>;
-}
+... (entire file already seen above)
